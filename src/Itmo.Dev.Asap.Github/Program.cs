0f1f5ca0565bffce2be90e29b77479b6742b0969
Roman Makarevich <remmybump@gmail.com>
2023-08-19 20:15:46 +0300

    feat: Added platform logging and YandexCloud

--- src/Itmo.Dev.Asap.Github/Program.cs
+++ src/Itmo.Dev.Asap.Github/Program.cs
@@ -1,3 +1,5 @@
+#pragma warning disable CA1506
+
 using Itmo.Dev.Asap.Github.Application.Extensions;
 using Itmo.Dev.Asap.Github.Application.Handlers.Extensions;
 using Itmo.Dev.Asap.Github.Caching.Extensions;
@@ -7,9 +9,16 @@ using Itmo.Dev.Asap.Github.Octokit.Extensions;
 using Itmo.Dev.Asap.Github.Presentation.Grpc.Extensions;
 using Itmo.Dev.Asap.Github.Presentation.Kafka.Extensions;
 using Itmo.Dev.Asap.Github.Presentation.Webhooks.Extensions;
+using Itmo.Dev.Platform.Logging.Extensions;
+using Itmo.Dev.Platform.YandexCloud.Extensions;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddUserSecrets<Program>();
+
+await builder.AddYandexCloudConfigurationAsync();
+
+builder.AddPlatformSentry();
+builder.Host.AddPlatformSerilog(builder.Configuration);
 
 builder.Services
     .AddApplication()
@@ -39,6 +48,7 @@ app.UseSwaggerUI();
 app.UseCors(o => o.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 
 app.UseRouting();
+app.UsePlatformSentryTracing(builder.Configuration);
 
 app.UseGrpcPresentation();
 app.UseWebhooksPresentation();
