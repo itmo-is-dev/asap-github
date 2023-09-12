8b95bd70695677604ffe994d932bbf3e9f8b1cfd
ronimizy <ronimizy@outlook.com>
2023-09-13 00:26:03 +0300

    fix: mapping

--- src/Presentation/Itmo.Dev.Asap.Github.Presentation.Grpc.Mapping/GithubUserServiceMapper.cs
+++ src/Presentation/Itmo.Dev.Asap.Github.Presentation.Grpc.Mapping/GithubUserServiceMapper.cs
@@ -1,5 +1,7 @@
 using Itmo.Dev.Asap.Github.Application.Contracts.Users.Commands;
 using Itmo.Dev.Asap.Github.Application.Contracts.Users.Queries;
+using Itmo.Dev.Asap.Github.Application.Dto.Users;
+using Itmo.Dev.Asap.Github.Models;
 using Itmo.Dev.Asap.Github.Users;
 using Riok.Mapperly.Abstractions;
 
@@ -13,4 +15,8 @@ internal static partial class GithubUserServiceMapper
     public static partial UpdateGithubUsernames.Command MapTo(this UpdateUsernameRequest request);
 
     public static partial FindByIdsResponse MapFrom(this FindUsersByIds.Response response);
+
+    [MapProperty(nameof(GithubUserDto.Id), nameof(GithubUser.UserId))]
+    [MapProperty(nameof(GithubUserDto.GithubUsername), nameof(GithubUser.Username))]
+    private static partial GithubUser MapToGithubUser(GithubUserDto user);
 }
