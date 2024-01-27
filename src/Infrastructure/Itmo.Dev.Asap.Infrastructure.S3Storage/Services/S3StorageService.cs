43052ab4c0bb937bc97a7411dd06ed63fd13df02
ronimizy <ronimizy@outlook.com>
2024-01-27 20:10:52 +0300

    chore: added file link ttl config

--- src/Infrastructure/Itmo.Dev.Asap.Infrastructure.S3Storage/Services/S3StorageService.cs
+++ src/Infrastructure/Itmo.Dev.Asap.Infrastructure.S3Storage/Services/S3StorageService.cs
@@ -11,14 +11,17 @@ public class S3StorageService : IStorageService, IDisposable
 {
     private readonly AmazonS3Client _client;
     private readonly IDateTimeProvider _dateTimeProvider;
+    private readonly S3StorageOptions _options;
 
-    public S3StorageService(IOptionsMonitor<S3StorageOptions> optionsMonitor, IDateTimeProvider dateTimeProvider)
+    public S3StorageService(
+        IOptionsMonitor<S3StorageOptions> optionsMonitor,
+        IDateTimeProvider dateTimeProvider)
     {
         _dateTimeProvider = dateTimeProvider;
-        S3StorageOptions options = optionsMonitor.CurrentValue;
+        _options = optionsMonitor.CurrentValue;
 
-        var config = new AmazonS3Config { ServiceURL = options.ServiceUrl.ToString() };
-        _client = new AmazonS3Client(options.KeyId, options.SecretAccessKey, config);
+        var config = new AmazonS3Config { ServiceURL = _options.ServiceUrl.ToString() };
+        _client = new AmazonS3Client(_options.KeyId, _options.SecretAccessKey, config);
     }
 
     public async Task<StoredData> StoreAsync(string bucketName, Stream content, CancellationToken cancellationToken)
@@ -39,7 +42,7 @@ public class S3StorageService : IStorageService, IDisposable
         {
             Key = key,
             BucketName = bucketName,
-            Expires = _dateTimeProvider.Current.AddHours(48).DateTime,
+            Expires = _dateTimeProvider.Current.Add(_options.LinkTimeToLive).DateTime,
         };
 
         string link = _client.GetPreSignedURL(preSignRequest);
